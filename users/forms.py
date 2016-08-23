7f37ce6359602fe169af8d4f95e6d4185f94a25f
carlosribas <caduribas@gmail.com>
2016-08-23 17:29:36 -0300

    Comments erased

--- users/forms.py
+++ users/forms.py
@@ -54,15 +54,6 @@ class UserChangeForm(forms.ModelForm):
         return self.initial["password"]
 
 
-# class SignupForm(forms.Form):
-#     first_name = forms.CharField(max_length=30, label='Voornaam')
-#     last_name = forms.CharField(max_length=30, label='Achternaam')
-#
-#     def signup(self, request, user):
-#         user.first_name = self.cleaned_data['first_name']
-#         user.last_name = self.cleaned_data['last_name']
-#         user.save()
-#
 class SignupForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
