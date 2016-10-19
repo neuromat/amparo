166649e14e373037de4730f7b07310b67991c2cf
carlosribas <caduribas@gmail.com>
2016-10-19 17:18:21 -0200

    Send email updated

--- users/views.py
+++ users/views.py
@@ -112,55 +112,48 @@ def list_of_users(request):
 @login_required
 def send_email_to_users(request):
     if request.user.has_perm('users.send_email_to_users'):
-        lectures = Blog.objects.active_translations()
-        lectures = lectures.filter(banner=True)
-
         if request.method == 'POST':
-            selected_lecture = request.POST['lecture']
+            selected_target = request.POST['target']
             subject_typed = request.POST['subject']
             message_typed = request.POST['message']
-            get_lecture = lectures.get(id=selected_lecture)
-            lecture_title = get_lecture.title
             users = CustomUser.objects.all()
             list_of_emails = []
+            text_content = message_typed
+            html_content = message_typed
 
-            if not subject_typed:
-                subject_typed = _('Lecture from Rede AMPARO')
-
-            if not message_typed:
-                text_content = _('The lecture %s will start soon') % lecture_title
-                html_content = _('<p>The lecture %s will start soon.</p>') % lecture_title
-            else:
-                text_content = message_typed
-                html_content = message_typed
-
-            if get_lecture.publish:
+            if selected_target == '2':
                 for user in users:
                     if user.email:
                         list_of_emails.append(user.email)
-            else:
+            elif selected_target == '1':
                 for user in users.filter(type_of_person__name='Profissional'):
                     if user.email:
                         list_of_emails.append(user.email)
 
-            subject, from_email, to = subject_typed, settings.EMAIL_HOST_USER, list_of_emails
+            if subject_typed and message_typed and list_of_emails:
+                subject, from_email, to = subject_typed, settings.EMAIL_HOST_USER, list_of_emails
 
-            if subject and from_email and to:
-                connection = mail.get_connection()
-                connection.open()
+                if subject and from_email and to:
+                    connection = mail.get_connection()
+                    connection.open()
 
-                try:
-                    msg = EmailMultiAlternatives(subject, text_content, from_email, bcc=to)
-                    msg.attach_alternative(html_content, "text/html")
-                    msg.send()
-                except BadHeaderError:
-                    return HttpResponse('Invalid header found.')
+                    try:
+                        msg = EmailMultiAlternatives(subject, text_content, from_email, bcc=to)
+                        msg.attach_alternative(html_content, "text/html")
+                        if request.FILES:
+                            file_uploaded = request.FILES['attachment']
+                            msg.attach(file_uploaded.name, file_uploaded.read(), file_uploaded.content_type)
+                        msg.send()
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
 
-                connection.close()
-                messages.success(request, _('Email successfully sent!'))
+                    connection.close()
+                    messages.success(request, _('Email successfully sent!'))
+            else:
+                messages.error(request, _('You must select the target audience and fill in the subject and message '
+                                          'fields'))
 
-        context = {'lectures': lectures}
-        return render(request, 'users/send_email.html', context)
+        return render(request, 'users/send_email.html')
 
     else:
         return render(request, '404.html')
